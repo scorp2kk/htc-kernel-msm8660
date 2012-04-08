24202b5f16fb5934bd3a8f198cc694ac5c80313a
scorp2kk <scorp2kk@gmail.com>
2012-04-08 03:08:34 +0200

    Revert "msm_fb: display: HDMI power reduction at suspend"
    This reverts commit a9d7c8e55195d3c4f9ee5ab22fee315637abfa47.

--- drivers/video/msm/hdmi_msm.c
+++ drivers/video/msm/hdmi_msm.c
@@ -3505,19 +3505,6 @@ static int hdmi_msm_hpd_on(bool trigger_handler)
 	return 0;
 }
 
-static int hdmi_msm_power_ctrl(boolean enable)
-{
-	if (!external_common_state->hpd_feature_on)
-		return 0;
-
-	if (enable)
-		hdmi_msm_hpd_on(true);
-	else
-		hdmi_msm_hpd_off();
-
-	return 0;
-}
-
 static int hdmi_msm_power_on(struct platform_device *pdev)
 {
 	struct msm_fb_data_type *mfd = platform_get_drvdata(pdev);
@@ -3549,7 +3536,7 @@ static int hdmi_msm_power_on(struct platform_device *pdev)
 #endif /* CONFIG_FB_MSM_HDMI_MSM_PANEL_HDCP_SUPPORT */
 
 	changed = hdmi_common_get_video_format_from_drv_data(mfd);
-	if (!external_common_state->hpd_feature_on || mfd->ref_cnt) {
+	if (!external_common_state->hpd_feature_on) {
 		int rc = hdmi_msm_hpd_on(true);
 		DEV_INFO("HPD: panel power without 'hpd' feature on\n");
 		if (rc) {
@@ -3588,8 +3575,6 @@ static int hdmi_msm_power_on(struct platform_device *pdev)
  */
 static int hdmi_msm_power_off(struct platform_device *pdev)
 {
-	struct msm_fb_data_type *mfd = platform_get_drvdata(pdev);
-
 	if (!hdmi_msm_state->hdmi_app_clk)
 		return -ENODEV;
 #ifdef CONFIG_SUSPEND
@@ -3632,7 +3617,7 @@ static int hdmi_msm_power_off(struct platform_device *pdev)
 	}
 
 	mutex_lock(&external_common_state_hpd_mutex);
-	if (!external_common_state->hpd_feature_on || mfd->ref_cnt)
+	if (!external_common_state->hpd_feature_on)
 		hdmi_msm_hpd_off();
 	mutex_unlock(&external_common_state_hpd_mutex);
 
@@ -3946,7 +3931,6 @@ static struct platform_driver this_driver = {
 static struct msm_fb_panel_data hdmi_msm_panel_data = {
 	.on = hdmi_msm_power_on,
 	.off = hdmi_msm_power_off,
-	.power_ctrl = hdmi_msm_power_ctrl,
 };
 
 static struct platform_device this_device = {
